a432896b8659e5e201ad5eac49ef1225bff63073
Gianluca <gbelardelli@gmail.com>
2023-12-01 15:49:14 +0100

    add types and key manager

---
+++ autoload/key_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+const KEY_FURNITURE:String = "furnitures"
+const KEY_USE_FURNITURE:String = "use_furnitures"
+const KEY_FURNITURE_COUNT:String = "furniture_count"
+const KEY_CARDS:String = "cards"
+const KEY_MONSTERS:String = "monsters"
+const KEY_MONSTERS_CLASS:String = "monsters_class"
+const KEY_OPTIONS:String = "game_options"
+const KEY_GAME_TOKEN:String = "game_token"
+const KEY_GAME_TYPE:String = "game_type"
+const KEY_GAME_BOARD:String = "game_board"
